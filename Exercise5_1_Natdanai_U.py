2c3e0e49a703aeb0f0246af4d64f8f92df7535c1
Ken <nat180234@gmail.com>
2021-06-12 21:27:53 +0700

    Exercise5_1

---
+++ Exercise5_1_Natdanai_U.py
@@ -0,0 +1,7 @@
+A = int(input("FirstNumber")) #FirstNumber
+B = int(input("SecondNumber")) #SecondNumber
+
+print (A,"+",B,"=",A+B)
+print (A,"-",B,"=",A-B)
+print (A,"*",B,"=",A*B)
+print (A,"/",B,"=",A/B)
---
+++ Exercise5_2_Natdanai_U.py
@@ -0,0 +1,5 @@
+s = float(input("displacement"))
+t = float(input("time"))
+v = s/t #average velocity (m/s หรือ km/hr)
+
+print(v,"km/h")
